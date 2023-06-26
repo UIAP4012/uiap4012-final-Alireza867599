5b4fa2cbfcd809afee1a30f6d3b0ed2a50874e9b
Alireza867599 <111694085+Alireza867599@users.noreply.github.com>
2023-06-27 00:38:55 +0330

    Update currency class

--- project.cpp
+++ project.cpp
@@ -1,58 +1,20 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <ctime>
-#define sep cout<<"____________________________________________________________"<<endl;
-using namespace std;
+class CurrencyConverter {
+    public:
 
-class User {
-public:
-    string username;
-    string password;
-
-    User(string username, string password) {
-        this->username = username;
-        this->password = password;
-    }
-};
-
-bool user_exists(string username, string password) {
-    ifstream file("d:\\project2\\users.txt");
-    string line;
-    while (getline(file, line)) {
-        if (line == "user name is :"+username + " and password is : " + password) {
-            return true;
+        double convertUsdToEur(double usd) {
+            return usd * usdToEurRate;
         }
-    }
 
-    return false;
-}
-unsigned int hash_password(const string& password) {
-    unsigned int hash = 0;
-    for (char c : password) {
-        hash = c + (hash << 6) + (hash << 16) - hash;
-    }
-    return hash;
-}
+        double convertEurToUsd(double eur) {
+            return eur / usdToEurRate;
+        }
+        void updateExchangeRate(double newRate) {
+            usdToEurRate = newRate;
+        }
+    private:
+        double usdToEurRate=0.85;
+    };
 
-void register_user() {
-    string username;
-    string password;
+    CurrencyConverter currency;
 
-    cout << "Enter username: ";
-    cin >> username;
 
-    cout << "Enter password: ";
-    cin >> password;
-    unsigned int hash(hash_password(password));
-    ofstream file1("d:\\project2\\hashpasswords.txt", ios::app);
-    file1<<"name of user :"<<username<<"\n"<< "hash password of user :"<<hash<<endl;
-    file1<<"_____________________________________________________________"<<endl;
-
-    User user(username, password);
-
-    ofstream file("d:\\project2\\users.txt", ios::app);
-    file<< "user name is :"<< user.username<< " and password is : " << user.password << endl;
-}
